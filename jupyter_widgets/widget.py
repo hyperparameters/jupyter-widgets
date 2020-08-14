4f032bd186bc09d7b0b81dd542a9967d4a354ac0
tushr <tusharkolhe@gofynd.com>
2020-08-14 13:55:14 +0530

    folder navigation dropdown

--- jupyter_widgets/widget.py
+++ jupyter_widgets/widget.py
@@ -1,18 +1,33 @@
 import os
 from matplotlib import pyplot as plt
 from ipywidgets import widgets,Layout
+import logging
 
 class ImageViewer:
     def __init__(self, img_dir):
         self.img_dir = img_dir
-        self.img_paths = [os.path.join(self.img_dir,name) for name in os.listdir(self.img_dir)]
+        self.img_paths, self._max_iterator = self._get_image_paths(self.img_dir)
+        self.sub_directory_options = self._get_sub_directories(self.img_dir)
         self._iterator = 0
-        self._max_iterator = len(self.img_paths)
         self._create_layout()
+        self._logger = logging.getLogger("ImageViewer")
+        
+    def _get_image_paths(self, directory):
+        image_paths=[os.path.join(directory,name) for name in os.listdir(directory) if name.split(".")[-1] in ["jpg","jpeg","png"]]
+        return image_paths, len(image_paths)
+    
+    def _get_sub_directories(self, directory):
+        s = [w for w in os.walk(directory)][0][1]
+        s.insert(0,"..")
+        sub_directories = []
+        for i, name in enumerate(s):
+            sub_directories.append((name,name))
+        #print(sub_directories)
+        return sub_directories
     
     def _create_layout(self):
         self.button_layout = Layout(width="40px",height="30px")
-        self.window_layout = Layout(width="45%",height="640px")        
+        
     def create_widgets(self):
         self.slider = widgets.IntSlider(min=0, max=self._max_iterator-1)
         self.play = widgets.Play(
@@ -28,12 +43,21 @@ class ImageViewer:
         widgets.jslink((self.play, 'value'), (self.slider, 'value'))
         self.slider.observe(self.handle_slider_change, names="value")
         
+        #folder selector
+        self.folder_selector = widgets.Dropdown(
+            options=self.sub_directory_options,
+            value=None,
+            description='folder:',layout=Layout(width="45%")
+        )
+        self.folder_selector.observe(self.handle_folder_selector_change, names="value")
+        
+        #image viewer
         image_base = self.read_image(0)
         self.image_viewer = widgets.Image(
                     value=image_base,
                     format='jpg',
                     width=640,
-                    height=320,layout=self.window_layout
+                    height=320,layout=Layout(width="45%",height="640px")
                 )
 
         self.next_button = widgets.Button(description='',
@@ -52,7 +76,7 @@ class ImageViewer:
 
         self.seek_bar = widgets.HBox([self.play,self.previous_button,self.next_button,self.slider])
         self.buttons = widgets.HBox([])
-        self.vbox = widgets.VBox([self.image_viewer,self.seek_bar,self.buttons])
+        self.vbox = widgets.VBox([self.folder_selector,self.image_viewer,self.seek_bar,self.buttons])
         return self.vbox
     
     def read_image(self, ind):
@@ -83,12 +107,32 @@ class ImageViewer:
         
     def handle_slider_change(self, change):
         ind = change.new
+        self._logger.debug(f"slider change {ind}")
+        if self._max_iterator>0:       
+            if ind >= self._max_iterator:
+                ind = 0
+    #             self.play.send_state()
+            print(f" handle slider change {ind}")
+            image = self.read_image(ind)
+            self.image_viewer.value = image
         
-        if ind >= self._max_iterator:
-            ind = 0
-            self.play.send_state()
-        image = self.read_image(ind)
-        self.image_viewer.value = image
+    def handle_folder_selector_change(self, change):
+        folder = change.new
+        if folder:
+            if folder != "..":
+
+                new_directory = os.path.join(self.img_dir,folder)
+            else:
+                new_directory = os.path.dirname(self.img_dir)
+
+            self.img_paths, self._max_iterator = self._get_image_paths(new_directory)
+            self.img_dir = new_directory
+            self.slider.value=1
+            self.slider.value=0
+            self.folder_selector.unobserve(self.handle_folder_selector_change, names="value")
+            self.folder_selector.options = self._get_sub_directories(new_directory)
+            self.folder_selector.observe(self.handle_folder_selector_change, names="value")
+            self.folder_selector.value= None
         
     def plot_img(self, path):
         plt.figure(figsize=(10,10))
